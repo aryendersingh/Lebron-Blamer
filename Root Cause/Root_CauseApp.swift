558abe05ada533c0b661cb413320e7fe6037163d
aryendersingh <aryendersingh2109@gmail.com>
2025-03-23 17:35:50 -0700

    Initial Commit

---
+++ Root Cause/Root_CauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Root_CauseApp.swift
+//  Root Cause
+//
+//  Created by Aryender Singh on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Root_CauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Root CauseTests/Root_CauseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Root_CauseTests.swift
+//  Root CauseTests
+//
+//  Created by Aryender Singh on 3/23/25.
+//
+
+import Testing
+@testable import Root_Cause
+
+struct Root_CauseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
